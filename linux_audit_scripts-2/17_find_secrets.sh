56516bba3f6309a5acb6198051c8b8300e946f72
Dhaval Prajapati <42002820+dhavz@users.noreply.github.com>
2025-07-24 15:50:17 -0400

    Update 17_find_secrets.sh

--- linux_audit_scripts-2/17_find_secrets.sh
+++ linux_audit_scripts-2/17_find_secrets.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-echo "[+] Searching for Potential Secrets in Readable Files"
+
+output="/tmp/found_secrets.txt"
 patterns='(password|passwd|secret|token|apikey|api_key|authorization)'
-find / -type f -readable -exec grep -IHinE "$patterns" {} \; 2>/dev/null
+
+echo "[+] Searching for Potential Secrets in Readable Files..."
+echo "[+] Output will be saved to $output"
+
+# Run the search and save output
+find / -type f -readable -exec grep -IHinE "$patterns" {} \; 2>/dev/null | tee "$output"
+
+echo
+echo "[✓] Done. Full results saved to: $output"
